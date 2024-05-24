61e08ebacbad5a20cb11fbc41ffe2ef181361236
Esther Pertishiya <51266860+Pertishiya@users.noreply.github.com>
2024-05-24 12:23:07 +0530

    Create SumOfTwoDigits.py

---
+++ SumOfTwoDigits.py
@@ -0,0 +1,8 @@
+print("Print sum of two digits")
+two_digit_number = input()
+first_digit = two_digit_number[0]
+second_digit = two_digit_number[1]
+
+sum = int(first_digit)+int(second_digit)
+
+print(sum)
