106f03319e08a59fd74bba714c29c6d995a78d58
Esther Pertishiya <51266860+Pertishiya@users.noreply.github.com>
2024-05-24 12:18:54 +0530

    Create max_contiguous_sub_array.py

---
+++ max_contiguous_sub_array.py
@@ -0,0 +1,13 @@
+def max_subarray_sum(arr):
+    max_sum = float('-inf')
+    current_sum = 0
+
+    for num in arr:
+        current_sum = max(num, current_sum + num)
+        max_sum = max(max_sum, current_sum)
+
+    return max_sum
+
+# Example usage:
+arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+print("Maximum sum of contiguous sub-array:", max_subarray_sum(arr))
